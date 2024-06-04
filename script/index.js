be6fccdc30b3f9aca8b6f2682d0b0606fca8df6e
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 14:44:25 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,25 @@
+let numbers = [];
+
+function addNumber() {
+    let numberInput = document.getElementById('numberInput').value;
+    if (numberInput !== "") {
+        numbers.push(Number(numberInput));
+        document.getElementById('numberInput').value = ""; // Clear input box
+        document.getElementById('array').innerHTML = "<p>The array of numbers is: " + numbers.join(", ") + "</p>";
+    }
+}
+
+function findMinMax() {
+    if (numbers.length === 0) {
+        document.getElementById('result').innerHTML = "<p>Please add some numbers first.</p>";
+        return;
+    }
+
+    let minNumber = Math.min(...numbers);
+    let maxNumber = Math.max(...numbers);
+
+    document.getElementById('result').innerHTML = "<p>The minimum number is: " + minNumber + "</p>";
+    document.getElementById('result').innerHTML += "<p>The maximum number is: " + maxNumber + "</p>";
+}
+
+
